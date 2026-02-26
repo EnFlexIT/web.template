804542e684723e0ce8301974be079c83442da1ce
alsmaraenflex <alsmaraenflex>
2026-02-26 13:02:53 +0100

    fix(featureFlags): enable devHome feature flag

--- src/redux/slices/featureFlags.ts
+++ src/redux/slices/featureFlags.ts
@@ -1,6 +1,6 @@
 export const menuFeatureFlags: Record<number, boolean> = {
   // Settings children
-  3011: false,   // devHome
+  3011: true,   // devHome
   3012: true,   // serverSettings
   3013: true,  // changePassword 
   3014: true,  // featureFlagForNewFeature
