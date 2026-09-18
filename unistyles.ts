31fe10c3f356be36169bd24383ec3042e0e7e0b0
alsmaraenflex <aalsamara@enflex.it>
2026-09-18 11:40:16 +0200

    feat: add application theme overrides

--- unistyles.ts
+++ unistyles.ts
@@ -1,34 +1,63 @@
-import { StyleSheet } from 'react-native-unistyles'
-import { lightTheme}from "@/template/styles/lightTheme";
-import { darkTheme } from '@/template/styles/darkTheme';
+import {
+  StyleSheet,
+} from "react-native-unistyles";
 
+import {
+  lightTheme,
+} from "@/template/styles/lightTheme";
+
+import {
+  darkTheme,
+} from "@/template/styles/darkTheme";
+
+import {
+  applicationThemeOverrides,
+} from "@/application/generated/applicationTheme.generated";
 
 const appThemes = {
-    light: lightTheme,
-    dark: darkTheme
-}
+  light: {
+    ...lightTheme,
+
+    colors: {
+      ...lightTheme.colors,
+      ...applicationThemeOverrides.light,
+    },
+  },
+
+  dark: {
+    ...darkTheme,
+
+    colors: {
+      ...darkTheme.colors,
+      ...applicationThemeOverrides.dark,
+    },
+  },
+};
 
 const breakpoints = {
-    xs: 0,
-    sm: 300,
-    md: 500,
-    lg: 800,
-    xl: 1200,
-}
+  xs: 0,
+  sm: 300,
+  md: 500,
+  lg: 800,
+  xl: 1200,
+};
 
-type AppBreakpoints = typeof breakpoints
-type AppThemes = typeof appThemes
+type AppBreakpoints =
+  typeof breakpoints;
 
-declare module 'react-native-unistyles' {
-    export interface UnistylesThemes extends AppThemes { }
-    export interface UnistylesBreakpoints extends AppBreakpoints { }
+type AppThemes =
+  typeof appThemes;
+
+declare module "react-native-unistyles" {
+  export interface UnistylesThemes
+    extends AppThemes {}
+
+  export interface UnistylesBreakpoints
+    extends AppBreakpoints {}
 }
 
 StyleSheet.configure({
-  themes: {
-    light: lightTheme,
-    dark: darkTheme,
-  },
+  themes: appThemes,
 
   breakpoints,
 
